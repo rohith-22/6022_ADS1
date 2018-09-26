1ef0d07f595aa40435b8dfde8421ff1218346553
rohith-22 <rohithkottauppari@msitprogram.net>
2018-09-26 21:40:52 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- ADS-1-Assignments/m3/3-sum/Solution.java
+++ ADS-1-Assignments/m3/3-sum/Solution.java
@@ -12,7 +12,7 @@ class ThreeSum {
 				} else if (arrayOfNumbers[i] + arrayOfNumbers[j] + arrayOfNumbers[k] > 0) {
 					k -= 1;
 				} else {
-					j += 1;
+					j = 1;
 					k -= 1;
 					countOfThreeSums += 1;
 				}
