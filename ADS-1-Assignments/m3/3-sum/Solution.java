d6c0567e7e14c95f9018d1c93bf86f84acd39136
rohith-22 <rohithkottauppari@msitprogram.net>
2018-09-26 21:38:05 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

---
+++ ADS-1-Assignments/m3/3-sum/Solution.java
@@ -0,0 +1,34 @@
+import java.util.*;
+class ThreeSum {
+	public int  count(int[] arrayOfNumbers) {
+		int countOfThreeSums = 0, j = 0, k = 0;
+		for (int i = 0; i < arrayOfNumbers.length; i++) {
+			j = i + 1;
+			k = arrayOfNumbers.length - 1;
+			while (j < k) {
+				if (arrayOfNumbers[i] + arrayOfNumbers[j] + arrayOfNumbers[k] < 0) {
+					j += 1;
+				} else if (arrayOfNumbers[i] + arrayOfNumbers[j] + arrayOfNumbers[k] > 0) {
+					k -= 1;
+				} else {
+					j += 1;
+					k -= 1;
+					countOfThreeSums += 1;
+				}
+			}
+		}
+		return countOfThreeSums;
+	}
+}
+public class Solution {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		ThreeSum objectThreeSum = new ThreeSum();
+		int[] arrayOfNumbers = new int[sc.nextInt()];
+		for (int i = 0; i < arrayOfNumbers.length; i++) {
+			arrayOfNumbers[i] = sc.nextInt();
+		}
+		Arrays.sort(arrayOfNumbers);
+		System.out.println(objectThreeSum.count(arrayOfNumbers));
+	}
+}
