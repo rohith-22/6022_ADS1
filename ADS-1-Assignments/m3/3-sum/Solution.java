82e67a2e0bb416aa6d0770c6c5ce04b4e2f19dc1
rohith-22 <rohithkottauppari@msitprogram.net>
2018-09-26 21:48:07 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- ADS-1-Assignments/m3/3-sum/Solution.java
+++ ADS-1-Assignments/m3/3-sum/Solution.java
@@ -1,35 +1,53 @@
 import java.util.Scanner;
 import java.util.Arrays;
+/**
+ * Class to calculate three sum.
+ */
 class ThreeSum {
-	public int  count(int[] arrayOfNumbers) {
-		int countOfThreeSums = 0, j = 0, k = 0;
-		for (int i = 0; i < arrayOfNumbers.length; i++) {
-			j = i + 1;
-			k = arrayOfNumbers.length - 1;
-			while (j < k) {
-				if (arrayOfNumbers[i] + arrayOfNumbers[j] + arrayOfNumbers[k] < 0) {
-					j += 1;
-				} else if (arrayOfNumbers[i] + arrayOfNumbers[j] + arrayOfNumbers[k] > 0) {
-					k -= 1;
-				} else {
-					// j = 1;
-					k -= 1;
-					countOfThreeSums += 1;
-				}
-			}
-		}
-		return countOfThreeSums;
-	}
+  /**
+   * count function givs the count of the threesum pairs
+   *
+   * @param      arrayOfNumbers  The array of numbers
+   *
+   * @return     returns the threesum count in the array
+   */
+  public int  count(final int[] arrayOfNumbers) {
+    int countOfThreeSums = 0, j = 0, k = 0;
+    for (int i = 0; i < arrayOfNumbers.length; i++) {
+      j = i + 1;
+      k = arrayOfNumbers.length - 1;
+      while (j < k) {
+        if (arrayOfNumbers[i] + arrayOfNumbers[j] + arrayOfNumbers[k] < 0) {
+          j++;
+        } else if (arrayOfNumbers[i] + arrayOfNumbers[j] + arrayOfNumbers[k] > 0) {
+          k--;
+        } else {
+          j++;
+          k--;
+          countOfThreeSums += 1;
+        }
+      }
+    }
+    return countOfThreeSums;
+  }
 }
+/**
+ * Class for solution.
+ */
 public class Solution {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		ThreeSum objectThreeSum = new ThreeSum();
-		int[] arrayOfNumbers = new int[sc.nextInt()];
-		for (int i = 0; i < arrayOfNumbers.length; i++) {
-			arrayOfNumbers[i] = sc.nextInt();
-		}
-		Arrays.sort(arrayOfNumbers);
-		System.out.println(objectThreeSum.count(arrayOfNumbers));
-	}
+  /**
+   * main function.
+   *
+   * @param      args  The arguments
+   */
+  public static void main(final String[] args) {
+    Scanner sc = new Scanner(System.in);
+    ThreeSum objectThreeSum = new ThreeSum();
+    int[] arrayOfNumbers = new int[sc.nextInt()];
+    for (int i = 0; i < arrayOfNumbers.length; i++) {
+      arrayOfNumbers[i] = sc.nextInt();
+    }
+    Arrays.sort(arrayOfNumbers);
+    System.out.println(objectThreeSum.count(arrayOfNumbers));
+  }
 }
