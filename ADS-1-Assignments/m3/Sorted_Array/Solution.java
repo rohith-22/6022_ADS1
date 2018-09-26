e51f185f8cfb7d8cb5a5bfcd4840fdd3c295b086
rohith-22 <rohithkottauppari@msitprogram.net>
2018-09-26 20:49:30 +0530

    "check_style_score -> 1 of 3 passed. style score: 0/1 "

--- ADS-1-Assignments/m3/Sorted_Array/Solution.java
+++ ADS-1-Assignments/m3/Sorted_Array/Solution.java
@@ -1,25 +1,42 @@
 import java.util.*;
 public class Solution {
-  public static int[] intArray(final String s) {
-    if (s.equals("")) {
-      return new int[0];
-    }
-    return Arrays.stream(s.split(","))
-           .mapToInt(Integer::parseInt)
-           .toArray();
-  }
-  public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
-    int[]firstArray = new int[sc.nextInt()];
-    int[]secondArray = new int[sc.nextInt()];
-    firstArray = intArray(sc.next());
-    secondArray = intArray(sc.next());
-    // System.out.println(Arrays.toString(firstArray));
-    // System.out.println(Arrays.toString(secondArray));
-    int[] resultArray = new int[firstArray.length + secondArray.length];
-    System.arraycopy(firstArray, 0, resultArray, 0, firstArray.length);
-    System.arraycopy(secondArray, 0, resultArray, firstArray.length, secondArray.length);
-    Arrays.sort(resultArray);
-    System.out.println(Arrays.toString(resultArray).replace("[","").replace("]","").replace(" ",""));
-  }
+	public static int[] intArray(final String s) {
+		return Arrays.stream(s.split(","))
+		       .mapToInt(Integer::parseInt)
+		       .toArray();
+	}
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int[]firstArray = new int[sc.nextInt()];
+		int[]secondArray = new int[sc.nextInt()];
+		if (firstArray.length != 0) {
+			firstArray = intArray(sc.next());
+		} if (secondArray.length != 0) {
+			secondArray = intArray(sc.next());
+		}
+		int[] resultArray = new int[firstArray.length + secondArray.length];
+		int i = 0, j = 0;
+		while (i < firstArray.length && j < secondArray.length) {
+			if (firstArray[i] < secondArray[j]) {
+				resultArray[i++] = firstArray[i];
+				i = i + 1;
+			} else if (firstArray[i] > secondArray[j]) {
+				resultArray[i++] = secondArray[j];
+				j = j + 1;
+			}
+		}
+		if (firstArray.length > secondArray.length) {
+			while (i < firstArray.length + secondArray.length) {
+				resultArray[i++] = firstArray[i++];
+			}
+
+		}
+		if (secondArray.length > firstArray.length) {
+			while (i < firstArray.length + secondArray.length) {
+				resultArray[i++] = secondArray[j++];
+			}
+		}
+		System.out.println(Arrays.toString(resultArray).replace("[", "").replace("]", "").replace(" ", ""));
+
+	}
 }
