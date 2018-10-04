566995676cc95d0b769fa0d522ead0f6632370d2
rohith-22 <rohithkottauppari@msitprogram.net>
2018-10-04 20:20:31 +0530

    "check_style_score -> 1 of 3 passed. style score: 0/1 "

---
+++ ADS-1-Assignments/m10/10.1 LinkedList using Recursion/Solution.java
@@ -0,0 +1,96 @@
+import java.util.Scanner;
+class LinkedList {
+
+  private class Node {
+
+    int data;
+    Node next;
+
+    Node() {}
+
+
+    Node(int data) {
+      this.data = data;
+      this.next = next;
+    }
+  }
+
+  private Node head;
+  private Node tail;
+  private int size;
+  LinkedList() {
+    this.head = null;
+    this.tail = null;
+    this.size = 0;
+  }
+  public void insertAt(int index, int value) throws Exception {
+
+    if (index < 0 || index >= size) {
+      throw new Exception ("Can't insert at this position.");
+    }
+    head = insertAt(head, new Node(value), index, 0);
+    size++;
+  }
+  private Node insertAt(Node curr, Node newNode, int index, int currIndex) {
+    if (curr == null) {
+      return newNode;
+    } else if (currIndex == index) {
+      newNode.next = curr;
+      return newNode;
+    }
+
+    curr.next = insertAt(curr.next, newNode, index, currIndex + 1);
+    return curr;
+
+  }
+
+  public void reverse() throws Exception {
+    if (size == 0) {
+      throw new Exception("No elements to reverse.");
+    }
+    reverse(head, null);
+
+  }
+  private Node reverse(Node curr, Node prev) {
+
+
+
+    if (curr.next == null) {
+      head = curr;
+      head.next = prev;
+      return null;
+    }
+
+    Node temp = curr.next;
+    curr.next = prev;
+
+    reverse(temp, curr);
+    return head;
+  }
+}
+public class Solution {
+
+  public static void main(String[] args) {
+    LinkedList object = new LinkedList();
+    Scanner sc = new Scanner(System.in);
+    while (sc.hasNext()) {
+      String[] tokens = sc.nextLine().split(" ");
+      switch (tokens[0]) {
+      case "insertAt" :
+        try {
+          object.insertAt(Integer.parseInt(tokens[1]), Integer.parseInt(tokens[2]));
+          System.out.println(object);
+        } catch (Exception e) {
+          System.out.println(e.getMessage());
+        }
+      case "reverse":
+        try {
+          object.reverse();
+          System.out.println(object);
+        } catch (Exception e) {
+          System.out.println(e.getMessage());
+        }
+      }
+    }
+  }
+}
