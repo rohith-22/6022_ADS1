11763566900af581dae8abee2d1666aac828faf7
rohith-22 <rohithkottauppari@msitprogram.net>
2018-10-12 16:07:18 +0530

    "taxi_cab_numbers: 5/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-1-Assignments/m17/TaxiCabNumbers/Solution.java
+++ ADS-1-Assignments/m17/TaxiCabNumbers/Solution.java
@@ -1,90 +1,134 @@
 import java.util.Scanner;
-
+/**
+ * Class for cube sum.
+ */
 class CubeSum implements Comparable<CubeSum> {
-
-    private final int sum;
-
-    private final int first;
-    private final int second;
-    CubeSum(final int firstNumber, final int secondNumber) {
-        sum = firstNumber * firstNumber * firstNumber
-                   + secondNumber * secondNumber * secondNumber;
-        first = firstNumber;
-        second = secondNumber;
+  /**
+   *  variable sum.
+   */
+  private final int sum;
+  /**
+   * variable first.
+   */
+  private final int first;
+  /**
+   * variable second.
+   */
+  private final int second;
+  /**
+   * Constructs the object.
+   *
+   * @param      firstNumber   The first number
+   * @param      secondNumber  The second number
+   */
+  CubeSum(final int firstNumber, final int secondNumber) {
+    sum = firstNumber * firstNumber * firstNumber
+          + secondNumber * secondNumber * secondNumber;
+    first = firstNumber;
+    second = secondNumber;
+  }
+  /**
+   * Gets the sum.
+   *
+   * @return     The sum.
+   */
+  public int getSum() {
+    return sum;
+  }
+  /**
+   * Gets the first.
+   *
+   * @return     The first.
+   */
+  public int getFirst() {
+    return first;
+  }
+  /**
+   * Gets the second.
+   *
+   * @return     The second.
+   */
+  public int getSecond() {
+    return second;
+  }
+  /**
+   * compaers the sum of both objects.
+   *
+   * @param      other  The other
+   *
+   * @return     int
+   */
+  public int compareTo(final CubeSum other) {
+    if (sum < other.sum) {
+      return -1;
     }
-    public int getSum() {
-        return sum;
-    }
-    public int getFirst() {
-        return first;
+    if (sum > other.sum) {
+      return +1;
     }
 
-    public int getSecond() {
-        return second;
-    }
-
-    public int compareTo(final CubeSum other) {
-        if (sum < other.sum) {
-            return -1;
-        }
-        if (sum > other.sum) {
-            return +1;
-        }
-
-        return 0;
-    }
-
-    public String toString() {
-        return sum + " = " + first + "^3" + " + " + second + "^3";
-    }
+    return 0;
+  }
+  /**
+   * Returns a string representation of the object.
+   *
+   * @return     String representation of the object.
+   */
+  public String toString() {
+    return sum + " = " + first + "^3" + " + " + second + "^3";
+  }
 
 
 
 }
-
+/**
+ * class Solution.
+ */
 final class Solution {
+  /**
+   * Constructs the object.
+   */
+  private Solution() {
 
+  }
+  /**
+   * main function to handle testcases.
+   *
+   * @param      args  The arguments
+   */
+  public static void main(final String[] args) {
+    Scanner scan = new Scanner(System.in);
+    String[] tokens = scan.nextLine().split(" ");
+    int counter = 0;
+    final int limit = 600;
+    int nthNumber = Integer.parseInt(tokens[0]);
+    int combinations = Integer.parseInt(tokens[1]);
+    int tempSum = -1;
 
-
-    private Solution() {
-
+    MinPQ<CubeSum> pq = new MinPQ<CubeSum>();
+    for (int i = 0; i <= limit; i++) {
+      pq.insert(new CubeSum(i, i));
     }
 
-    public static void main(final String[] args) {
-        Scanner scan = new Scanner(System.in);
-        String[] tokens = scan.nextLine().split(" ");
-        int counter = 0;
-        final int limit = 600;
-        int nthNumber = Integer.parseInt(tokens[0]);
-        int combinations = Integer.parseInt(tokens[1]);
-        int tempSum = -1;
-
-        MinPQ<CubeSum> pq = new MinPQ<CubeSum>();
-        for (int i = 0; i <= limit; i++) {
-            pq.insert(new CubeSum(i, i));
+    while (!pq.isEmpty()) {
+      CubeSum s = pq.delMin();
+      if (tempSum == s.getSum()) {
+        counter++;
+      } else {
+        counter = 0;
+      }
+      if (counter == combinations - 1) {
+        nthNumber--;
+        if (nthNumber == 0) {
+          System.out.println(s.getSum());
+          break;
         }
+      }
 
-        while (!pq.isEmpty()) {
-            CubeSum s = pq.delMin();
-            if (tempSum == s.getSum()) {
-                counter++;
-            } else {
-                counter = 0;
-            }
-            if (counter == combinations - 1) {
-                nthNumber--;
-                if (nthNumber == 0) {
-                    System.out.println(s.getSum());
-                    break;
-                }
-            }
-
-            tempSum = s.getSum();
-            if (s.getSecond() < limit) {
-                pq.insert(new CubeSum(
-                              s.getFirst(), s.getSecond() + 1));
-            }
-        }
+      tempSum = s.getSum();
+      if (s.getSecond() < limit) {
+        pq.insert(new CubeSum(
+                    s.getFirst(), s.getSecond() + 1));
+      }
     }
-
+  }
 }
