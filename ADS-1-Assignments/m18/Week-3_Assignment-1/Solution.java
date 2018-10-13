bc10a4afc0ef6f6343ec70ddf366203feb05900c
rohith-22 <rohithkottauppari@msitprogram.net>
2018-10-13 12:43:39 +0530

    "ads1_week3_exam: 2/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-1-Assignments/m18/Week-3_Assignment-1/Solution.java
+++ ADS-1-Assignments/m18/Week-3_Assignment-1/Solution.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-import java.util.Comparator;
+// import java.util.Comparator;
 
 /**
  * Class for stock exchange.
@@ -23,6 +23,13 @@ class StockExchange implements Comparable<StockExchange> {
     nameOfCompany = name;
     percentChange = percentage;
   }
+  /**
+   * compare to method
+   *
+   * @param      that  The that
+   *
+   * @return     int
+   */
   public int compareTo(final StockExchange that) {
     if (percentChange > that.percentChange) {
       return 1;
@@ -68,6 +75,7 @@ public final class Solution {
     Scanner sc = new Scanner(System.in);
 
     int numberOfInputs = Integer.parseInt(sc.nextLine());
+    final int five = 5;
     /**
      * 9 o clock
      */
@@ -75,14 +83,17 @@ public final class Solution {
     MaxPQ<StockExchange>maxPqObj1 = new MaxPQ<>();
     for (int j = 0; j < numberOfInputs; j++) {
       String[] tokens = sc.nextLine().split(",");
-      minPqObj1.insert(new StockExchange(tokens[0], Double.parseDouble(tokens[1])));
-      maxPqObj1.insert(new StockExchange(tokens[0], Double.parseDouble(tokens[1])));
+      minPqObj1.insert(new StockExchange(tokens[0],
+                                         Double.parseDouble(tokens[1])));
+      maxPqObj1.insert(new StockExchange(tokens[0],
+                                         Double.parseDouble(tokens[1])));
+
     }
-    for (int i = 0; i < 5; i++) {
+    for (int i = 0; i < five; i++) {
       System.out.println(maxPqObj1.delMax());
     }
     System.out.println();
-    for (int i = 0; i < 5; i++) {
+    for (int i = 0; i < five; i++) {
       System.out.println(minPqObj1.delMin());
     }
     System.out.println();
@@ -95,15 +106,17 @@ public final class Solution {
      */
     for (int j = 0; j < numberOfInputs; j++) {
       String[] tokens = sc.nextLine().split(",");
-      minPqObj2.insert(new StockExchange(tokens[0], Double.parseDouble(tokens[1])));
-      maxPqObj2.insert(new StockExchange(tokens[0], Double.parseDouble(tokens[1])));
+      minPqObj2.insert(new StockExchange(tokens[0],
+                                         Double.parseDouble(tokens[1])));
+      maxPqObj2.insert(new StockExchange(tokens[0],
+                                         Double.parseDouble(tokens[1])));
     }
 
-    for (int i = 0; i < 5; i++) {
+    for (int i = 0; i < five; i++) {
       System.out.println(maxPqObj2.delMax());
     }
     System.out.println();
-    for (int i = 0; i < 5; i++) {
+    for (int i = 0; i < five; i++) {
       System.out.println(minPqObj2.delMin());
     }
     System.out.println();
@@ -114,14 +127,16 @@ public final class Solution {
     MaxPQ<StockExchange>maxPqObj3 = new MaxPQ<>();
     for (int j = 0; j < numberOfInputs; j++) {
       String[] tokens = sc.nextLine().split(",");
-      minPqObj3.insert(new StockExchange(tokens[0], Double.parseDouble(tokens[1])));
-      maxPqObj3.insert(new StockExchange(tokens[0], Double.parseDouble(tokens[1])));
+      minPqObj3.insert(new StockExchange(tokens[0],
+                                         Double.parseDouble(tokens[1])));
+      maxPqObj3.insert(new StockExchange(tokens[0],
+                                         Double.parseDouble(tokens[1])));
     }
-    for (int i = 0; i < 5; i++) {
+    for (int i = 0; i < five; i++) {
       System.out.println(maxPqObj3.delMax());
     }
     System.out.println();
-    for (int i = 0; i < 5; i++) {
+    for (int i = 0; i < five; i++) {
       System.out.println(minPqObj3.delMin());
     }
     System.out.println();
@@ -132,32 +147,36 @@ public final class Solution {
     MaxPQ<StockExchange>maxPqObj4 = new MaxPQ<>();
     for (int j = 0; j < numberOfInputs; j++) {
       String[] tokens = sc.nextLine().split(",");
-      minPqObj4.insert(new StockExchange(tokens[0], Double.parseDouble(tokens[1])));
-      maxPqObj4.insert(new StockExchange(tokens[0], Double.parseDouble(tokens[1])));
+      minPqObj4.insert(new StockExchange(tokens[0],
+                                         Double.parseDouble(tokens[1])));
+      maxPqObj4.insert(new StockExchange(tokens[0],
+                                         Double.parseDouble(tokens[1])));
     }
-    for (int i = 0; i < 5; i++) {
+    for (int i = 0; i < five; i++) {
       System.out.println(maxPqObj4.delMax());
     }
     System.out.println();
-    for (int i = 0; i < 5; i++) {
+    for (int i = 0; i < five; i++) {
       System.out.println(minPqObj4.delMin());
     }
     System.out.println();
-/**
-     * 13 o clock
-     */
+    /**
+         * 13 o clock
+         */
     MinPQ<StockExchange>minPqObj5 = new MinPQ<>();
     MaxPQ<StockExchange>maxPqObj5 = new MaxPQ<>();
     for (int j = 0; j < numberOfInputs; j++) {
       String[] tokens = sc.nextLine().split(",");
-      minPqObj5.insert(new StockExchange(tokens[0], Double.parseDouble(tokens[1])));
-      maxPqObj5.insert(new StockExchange(tokens[0], Double.parseDouble(tokens[1])));
+      minPqObj5.insert(new StockExchange(tokens[0],
+                                         Double.parseDouble(tokens[1])));
+      maxPqObj5.insert(new StockExchange(tokens[0],
+                                         Double.parseDouble(tokens[1])));
     }
-    for (int i = 0; i < 5; i++) {
+    for (int i = 0; i < five; i++) {
       System.out.println(maxPqObj5.delMax());
     }
     System.out.println();
-    for (int i = 0; i < 5; i++) {
+    for (int i = 0; i < five; i++) {
       System.out.println(minPqObj5.delMin());
     }
     System.out.println();
@@ -168,18 +187,19 @@ public final class Solution {
     MaxPQ<StockExchange>maxPqObj6 = new MaxPQ<>();
     for (int j = 0; j < numberOfInputs; j++) {
       String[] tokens = sc.nextLine().split(",");
-      minPqObj6.insert(new StockExchange(tokens[0], Double.parseDouble(tokens[1])));
-      maxPqObj6.insert(new StockExchange(tokens[0], Double.parseDouble(tokens[1])));
+      minPqObj6.insert(new StockExchange(tokens[0],
+                                         Double.parseDouble(tokens[1])));
+      maxPqObj6.insert(new StockExchange(tokens[0],
+                                         Double.parseDouble(tokens[1])));
     }
-    for (int i = 0; i < 5; i++) {
+    for (int i = 0; i < five; i++) {
       System.out.println(maxPqObj6.delMax());
     }
     System.out.println();
-    for (int i = 0; i < 5; i++) {
+    for (int i = 0; i < five; i++) {
       System.out.println(minPqObj6.delMin());
     }
     System.out.println();
-
 
   }
 }
