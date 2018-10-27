4051196a03c6c49b1b670130bc5dc9087b01444f
rohith-22 <rohithkottauppari@msitprogram.net>
2018-10-27 12:15:57 +0530

    "m24_a1_1: 4/4 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-1-Assignments/m24/1/Solution.java
+++ ADS-1-Assignments/m24/1/Solution.java
@@ -1,38 +1,55 @@
 import java.util.Scanner;
-public class Solution {
-	public static void main(final String[] args) {
-		Scanner scan = new Scanner(System.in);
-		int numberOfInputs = Integer.parseInt(scan.nextLine());
-		LinearProbingHashST<Integer, String> object = new LinearProbingHashST<Integer, String>();
-		for (int i = 0; i < numberOfInputs; i++) {
-			String[] tokens = scan.nextLine().split(",");
-			String string = tokens[1] + "&&" + tokens[2];
-			object.put(Integer.parseInt(tokens[0]), string);
-		}
-		int queries = Integer.parseInt(scan.nextLine());
-		for (int i = 0; i < queries; i++) {
-			String[] tokens = scan.nextLine().split(" ");
-			String studentInfo = object.get(Integer.parseInt(tokens[1]));
-			switch (tokens[2]) {
-			case "1":
-				if (studentInfo != null) {
-					String[] tokensOne = studentInfo.split("&&");
-					System.out.println(tokensOne[0]);
-				} else {
-					System.out.println("Student doesn't exists...");
-				}
-				break;
-			case "2":
-				if (studentInfo != null) {
-					String[] tokensOne = studentInfo.split("&&");
-					System.out.println(Double.parseDouble(tokensOne[1]));
-				} else {
-					System.out.println("Student doesn't exists...");
-				}
-				break;
-			}
+/**
+ * Class for solution.
+ */
+public final class Solution {
+  /**
+   * Constructs the object.
+   */
+  private Solution() {
+    /**
+     * empty constructor.
+     */
+  }
+  /**
+   * main solution to handle testcases.
+   * complexity of main method is O(1)
+   *
+   * @param      args  The arguments
+   */
+  public static void main(final String[] args) {
+    Scanner scan = new Scanner(System.in);
+    int numberOfInputs = Integer.parseInt(scan.nextLine());
+    LinearProbingHashST<Integer, String> object =
+      new LinearProbingHashST<Integer, String>();
+    for (int i = 0; i < numberOfInputs; i++) {
+      String[] tokens = scan.nextLine().split(",");
+      String string = tokens[1] + "&&" + tokens[2];
+      object.put(Integer.parseInt(tokens[0]), string);
+    }
+    int queries = Integer.parseInt(scan.nextLine());
+    for (int i = 0; i < queries; i++) {
+      String[] tokens = scan.nextLine().split(" ");
+      String studentInfo = object.get(Integer.parseInt(tokens[1]));
+      switch (tokens[2]) {
+      case "1":
+        if (studentInfo != null) {
+          String[] tokensOne = studentInfo.split("&&");
+          System.out.println(tokensOne[0]);
+        } else {
+          System.out.println("Student doesn't exists...");
+        }
+        break;
+      case "2":
+        if (studentInfo != null) {
+          String[] tokensOne = studentInfo.split("&&");
+          System.out.println(Double.parseDouble(tokensOne[1]));
+        } else {
+          System.out.println("Student doesn't exists...");
+        }
+        break;
+      }
 
-
-		}
-	}
+    }
+  }
 }
