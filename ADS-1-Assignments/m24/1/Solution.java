cc94852128f2788ffc0a94123477fc366672eea1
rohith-22 <rohithkottauppari@msitprogram.net>
2018-10-27 12:08:36 +0530

    "m24_a1_1: 3/4 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-1-Assignments/m24/1/Solution.java
+++ ADS-1-Assignments/m24/1/Solution.java
@@ -1,77 +1,38 @@
 import java.util.Scanner;
-
-// class Students implements Comparable<Students> {
-// 	/**
-// 	 * name variable.
-// 	 */
-// 	private String studentName;
-// 	/**
-// 	 * roll number variable.
-// 	 */
-// 	private int rollNumber;
-// 	/**
-// 	 * totalMarks variable.
-// 	 */
-// 	private Double totalMarks;
-// 	/**
-// 	 * Constructs the object.
-// 	 *
-// 	 * @param      roll  The  roll
-// 	 * @param      name    The name
-// 	 * @param      marks     The marks
-// 	 */
-// 	Students(final int roll, final String name,
-// 	         final Double marks) {
-// 		rollNumber = roll;
-// 		studentName = name;
-// 		totalMarks = marks;
-// 	}
-// 	/**
-// 	 * compares the two students marks objects using name.
-// 	 *
-// 	 * @param      obj   The object
-// 	 *
-// 	 * @return     returns int
-// 	 */
-// 	public int compareTo(final Students obj) {
-// 		return totalMarks.compareTo(obj.totalMarks);
-// 	}
-// 	/**.
-// 	   * Returns a string representation of the object.
-// 	   *
-// 	   * @return     String representation of the object.
-// 	   */
-// 	// public String toString() {
-// 	// 	return this.studentName;
-// 	// }
-// }
-public final class Solution {
-	private Solution() {
-
-	}
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int noOfInputs = Integer.parseInt(sc.nextLine());
-		BinarySearchTree<Integer, String> objectOneBST = new BinarySearchTree<>();
-		BinarySearchTree<Integer, Double>objectTwoBST = new BinarySearchTree<>();
-		for (int i = 0; i < noOfInputs; i++) {
-			String[] tokens = sc.nextLine().split(",");
-			objectOneBST.put(Integer.parseInt(tokens[0]), tokens[1]);
-			objectTwoBST.put(Integer.parseInt(tokens[0]), Double.parseDouble(tokens[2]));
-
+public class Solution {
+	public static void main(final String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int numberOfInputs = Integer.parseInt(scan.nextLine());
+		LinearProbingHashST<Integer, String> object = new LinearProbingHashST<Integer, String>();
+		for (int i = 0; i < numberOfInputs; i++) {
+			String[] tokens = scan.nextLine().split(",");
+			String string = tokens[1] + "&&" + tokens[2];
+			object.put(Integer.parseInt(tokens[0]), string);
 		}
-		int outputs = Integer.parseInt(sc.nextLine());
-		for (int i = 0; i < outputs; i++) {
-			String[] tokens = sc.nextLine().split(" ");
+		int queries = Integer.parseInt(scan.nextLine());
+		for (int i = 0; i < queries; i++) {
+			String[] tokens = scan.nextLine().split(" ");
+			String studentInfo = object.get(Integer.parseInt(tokens[1]));
 			switch (tokens[2]) {
 			case "1":
-				System.out.println(objectOneBST.get(Integer.parseInt(tokens[1])));
+				if (studentInfo != null) {
+					String[] tokensOne = studentInfo.split("&&");
+					System.out.println(tokensOne[0]);
+				} else {
+					System.out.println("Student doesn't exists...");
+				}
 				break;
 			case "2":
-				System.out.println(objectTwoBST.get(Integer.parseInt(tokens[1])));
+				if (studentInfo != null) {
+					String[] tokensOne = studentInfo.split("&&");
+					System.out.println(tokensOne[1]);
+				} else {
+					System.out.println("Student doesn't exists...");
+				}
 				break;
-
 			}
+
+
 		}
 	}
 }
