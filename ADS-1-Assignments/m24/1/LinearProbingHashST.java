aecfb429dfb9a1d42e19deee5e7a714808a206b5
rohith-22 <rohithkottauppari@msitprogram.net>
2018-10-27 12:59:25 +0530

    "m24_a1_1: 4/4 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- ADS-1-Assignments/m24/1/LinearProbingHashST.java
+++ ADS-1-Assignments/m24/1/LinearProbingHashST.java
@@ -9,9 +9,9 @@ public class LinearProbingHashST<Key, Value> {
    * { var_description }.
    */
   private static final int INIT_CAPACITY = 4;
-/**
- * { var_description }.
- */
+  /**
+   * { var_description }.
+   */
   private int n;          // number of key-value pairs in
   /**
    * { var_description }.
@@ -92,7 +92,8 @@ public class LinearProbingHashST<Key, Value> {
    * @return     { description_of_the_return_value }
    */
   private int hash(final Key key) {
-    return (key.hashCode() & 0x7fffffff) % m;
+    final int magic = 0x7fffffff;
+    return (key.hashCode() & magic) % m;
   }
 
   // resizes the hash table to the given capacity by re-hashi.
@@ -163,105 +164,105 @@ public class LinearProbingHashST<Key, Value> {
       IllegalArgumentException("argument to get() is null");
     }
     for (int i = hash(key); keys[i] != null; i = (i + 1) % m) {
-    if (keys[i].equals(key)) {
-      return vals[i];
+      if (keys[i].equals(key)) {
+        return vals[i];
+      }
     }
-  }
-  return null;
-}
-
-/**
- * Removes the specified key and its associated value from this symbol table
- * (if the key is in this symbol table).
- *
- * @param  key the key
- * @throws IllegalArgumentException if {@code key} is {@code null}
- */
-public void delete(final Key key) {
-  if (key == null) {
-    throw new
-    IllegalArgumentException("argument to delete() is null");
-  }
-  if (!contains(key)) {
-    return;
+    return null;
   }
 
-  // find position i of key
-  int i = hash(key);
-  while (!key.equals(keys[i])) {
-    i = (i + 1) % m;
-  }
+  /**
+   * Removes the specified key and its associated value from this symbol table
+   * (if the key is in this symbol table).
+   *
+   * @param  key the key
+   * @throws IllegalArgumentException if {@code key} is {@code null}
+   */
+  public void delete(final Key key) {
+    if (key == null) {
+      throw new
+      IllegalArgumentException("argument to delete() is null");
+    }
+    if (!contains(key)) {
+      return;
+    }
 
-  // delete key and associated value
-  keys[i] = null;
-  vals[i] = null;
+    // find position i of key
+    int i = hash(key);
+    while (!key.equals(keys[i])) {
+      i = (i + 1) % m;
+    }
 
-  // rehash all keys in same cluster
-  i = (i + 1) % m;
-  while (keys[i] != null) {
-    // delete keys[i] an vals[i] and reinsert
-    Key   keyToRehash = keys[i];
-    Value valToRehash = vals[i];
+    // delete key and associated value
     keys[i] = null;
     vals[i] = null;
-    n--;
-    put(keyToRehash, valToRehash);
+
+    // rehash all keys in same cluster
     i = (i + 1) % m;
-  }
-
-  n--;
-
-  // halves size of array if it's 12.5% full or less
-  if (n > 0 && n <= m / 2 * 2 * 2) {
-    resize(m / 2);
-  }
-
-  assert check();
-}
-
-/**
- * Returns all keys in this symbol table as an {@code Iterable}.
- * To iterate over all of the keys in the symbol table named {@code st},
- * use the foreach notation: {@code for (Key key : st.keys())}.
- *
- * @return all keys in this symbol table
- */
-public Iterable<Key> keys() {
-  Queue<Key> queue = new Queue<Key>();
-  for (int i = 0; i < m; i++) {
-    if (keys[i] != null) {
-      queue.enqueue(keys[i]);
+    while (keys[i] != null) {
+      // delete keys[i] an vals[i] and reinsert
+      Key   keyToRehash = keys[i];
+      Value valToRehash = vals[i];
+      keys[i] = null;
+      vals[i] = null;
+      n--;
+      put(keyToRehash, valToRehash);
+      i = (i + 1) % m;
     }
+
+    n--;
+
+    // halves size of array if it's 12.5% full or less
+    if (n > 0 && n <= m / 2 * 2 * 2) {
+      resize(m / 2);
+    }
+
+    assert check();
   }
-  return queue;
-}
+
+  /**
+   * Returns all keys in this symbol table as an {@code Iterable}.
+   * To iterate over all of the keys in the symbol table named {@code st},
+   * use the foreach notation: {@code for (Key key : st.keys())}.
+   *
+   * @return all keys in this symbol table
+   */
+  public Iterable<Key> keys() {
+    Queue<Key> queue = new Queue<Key>();
+    for (int i = 0; i < m; i++) {
+      if (keys[i] != null) {
+        queue.enqueue(keys[i]);
+      }
+    }
+    return queue;
+  }
 
 // integrity check - don't check after each put() because
 // integrity not maintained during a delete()
 
-/**
- * { function_description }.
- *
- * @return     { description_of_the_return_value }
- */
-private boolean check() {
+  /**
+   * { function_description }.
+   *
+   * @return     { description_of_the_return_value }
+   */
+  private boolean check() {
 
-  // check that hash table is at most 50% full
-  if (m < 2 * n) {
-    System.err.println("Hash table size m = " + m + "; array size n = " + n);
-    return false;
-  }
-
-  // check that each key in table can be found by get()
-  for (int i = 0; i < m; i++) {
-    if (keys[i] == null) {
-      continue;
-    } else if (get(keys[i]) != vals[i]) {
-      System.err.println(
-        "get[" + keys[i] + "] = " + get(keys[i]) + "; vals[i] = " + vals[i]);
+    // check that hash table is at most 50% full
+    if (m < 2 * n) {
+      System.err.println("Hash table size m = " + m + "; array size n = " + n);
       return false;
     }
+
+    // check that each key in table can be found by get()
+    for (int i = 0; i < m; i++) {
+      if (keys[i] == null) {
+        continue;
+      } else if (get(keys[i]) != vals[i]) {
+        System.err.println(
+          "get[" + keys[i] + "] = " + get(keys[i]) + "; vals[i] = " + vals[i]);
+        return false;
+      }
+    }
+    return true;
   }
-  return true;
-}
 }
