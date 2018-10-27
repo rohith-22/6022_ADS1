4c42e70ba3a1d67b5dc16f012881956c5b873383
rohith-22 <rohithkottauppari@msitprogram.net>
2018-10-27 13:11:40 +0530

    assignment2

---
+++ ADS-1-Assignments/m24/2/Solution.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+class Students {
+	int rankOfStudent;
+	String namOfStudent;
+	Double marksOfStudent;
+	Students(int rank, String name, Double marks) {
+		rankOfStudent = rank;
+		marksOfStudent = marks;
+		namOfStudent = name;
+	}
+}
+public final class Solution {
+	private Solution() {
+		public static void main(String[] args) {
+			Scanner scan = new Scanner(System.in);
+			
+		}
+	}
+}
